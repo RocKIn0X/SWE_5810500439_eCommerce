27be8c1818ca1471c7a39ba697afffdb39cd72e5
Dantai <dantai39@gmail.com>
2017-09-09 14:20:40 +0700

    can Hello World

--- src/profiles/admin.py
+++ src/profiles/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from.models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Profile
+
+admin.site.register(Profile, ProfileAdmin)
--- src/profiles/views.py
+++ src/profiles/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = locals()
+    template = 'home.html'
+    return render(request, template, context)
