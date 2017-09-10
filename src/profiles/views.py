3c8e58099842975eebae7a2c2059b5f0012403f2
Dantai <dantai39@gmail.com>
2017-09-10 19:35:48 +0700

    add forgot password and signup on home

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 def home(request):
@@ -11,4 +12,11 @@ def home(request):
 def about(request):
     context = {}
     template = 'about.html'
+    return render(request, template, context)
+
+@login_required
+def userProfile(request):
+    user = request.user
+    context = {'user': user}
+    template = 'profile.html'
     return render(request, template, context)
