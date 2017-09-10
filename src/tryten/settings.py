289d83bee8f20c309e0eb92b71c901e2707dc19b
Dantai <dantai39@gmail.com>
2017-09-10 17:13:25 +0700

    add contact page

--- src/tryten/settings.py
+++ src/tryten/settings.py
@@ -15,6 +15,12 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'gmail'
+EMAIL_HOST_PASSWORD = 'gmail password'
+EMAIL_PORT = '587'
+EMAIL_USE_TSL = True
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -38,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'profiles',
+    'contact',
 ]
 
 MIDDLEWARE = [
